ca397a0a54dc7bfa32c5c6fc3ea03c3bd266fa4d
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-12-18 16:23:11 +0100

    Add project files.

---
+++ PlayGroundLibForberedelse/PlayGround.cs
@@ -0,0 +1,18 @@
+﻿namespace PlayGroundLibForberedelse
+{
+    public class PlayGround
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxChildren { get; set; }
+        public int MinAge { get; set; }
+
+        // ToString metode
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, MaxChildren: {MaxChildren}, MinAge: {MinAge}";
+        }
+
+    }
+}
---
+++ PlayGroundLibForberedelse/PlayGroundsRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGroundLibForberedelse
+{
+    public class PlayGroundsRepository
+    {
+        private readonly List<PlayGround> _playGrounds; // Liste til PlayGround objekter
+        private int _nextId; // Variable til at tildele nye IDs
+
+        // Constructor
+        public PlayGroundsRepository()
+        {
+            _playGrounds = new List<PlayGround>
+            {
+                new PlayGround { Id = 1, Name = "Millpark", MaxChildren = 10, MinAge = 5 },
+                new PlayGround { Id = 2, Name = "Secret Playground", MaxChildren = 12, MinAge = 4 },
+                new PlayGround { Id = 3, Name = "Library", MaxChildren = 8, MinAge = 3 },
+                new PlayGround { Id = 4, Name = "School", MaxChildren = 15, MinAge = 7 }
+            };
+            _nextId = 5; // Startværdi for næste ID
+        }
+
+        // Returnerer alle PlayGround objekter
+        public List<PlayGround> GetAll()
+        {
+            return _playGrounds;
+        }
+
+        // Returnerer et PlayGround objekt med et bestemt ID, ellers null
+        public PlayGround GetById(int id)
+        {
+            return _playGrounds.FirstOrDefault(pg => pg.Id == id);
+        }
+
+        // Tilføjer et nyt PlayGround objekt og returnerer det tilføjede objekt
+        public PlayGround Add(PlayGround playGr)
+        {
+            playGr.Id = _nextId++; // Tildel nyt ID og opdater _nextId
+            _playGrounds.Add(playGr);
+            return playGr;
+        }
+
+        // Opdaterer et eksisterende PlayGround objekt med et bestemt ID
+        public PlayGround Update(int id, PlayGround updatedPlayGr)
+        {
+            var existingPlayGr = GetById(id);
+            if (existingPlayGr != null)
+            {
+                existingPlayGr.Name = updatedPlayGr.Name;
+                existingPlayGr.MaxChildren = updatedPlayGr.MaxChildren;
+                existingPlayGr.MinAge = updatedPlayGr.MinAge;
+            }
+            return existingPlayGr;
+        }
+
+    }
+}
